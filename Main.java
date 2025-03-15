42f74701d7e030d90694fe15d185642aec4227c1
Jacobo Blandón Castro <jacoboblandon94@gmail.com>
2025-03-15 14:34:06 -0500

    feat: add menu for universal set input and subsets check
    Added a menu system to allow users to input the size of a universal set
    and its elements. The program now checks if subsets exist based on user
    input. Includes a loop for repeated operations and an exit condition.

--- Main.java
+++ Main.java
@@ -1,5 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    showMenu();
+  }
+
+  public static void showMenu() {
+    Scanner sc = new Scanner(System.in);
+    String[] set;
+
+    while (true) {
+      System.out.println("Ingrese el tamaño del conjunto universal o 0 para salir: ");
+      int universalSize = sc.nextInt();
+      sc.nextLine();
+
+      if (universalSize == 0) {
+        break;
+      }
+
+      set = new String[universalSize];
+
+      for (int i = 0; i < universalSize; i++) {
+        System.out.println("Ingrese el elemento " + (i + 1) + " del conjunto universal: ");
+        String element = sc.nextLine();
+        set[i] = element;
+      }
+
+      System.out.println("¿existen subconjuntos? (s/n)");
+      String answer = sc.nextLine();
+
+      if (answer == "n") {
+        break;
+      }
+
+      System.out.println(Arrays.toString(set));
+    }
+    sc.close();
   }
 }
